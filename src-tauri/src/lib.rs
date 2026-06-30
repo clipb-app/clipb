54a2d6f8df1f2c61957d52dff9c34cce18dd8aff
Faruk Umar <f.umaridris.mail@gmail.com>
2026-06-30 18:33:42 +0100

    Show tray menu on tray click

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,6 @@
 use tauri::{
     menu::{Menu, MenuItem},
-    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
+    tray::TrayIconBuilder,
     Manager, WebviewUrl, WebviewWindow, WebviewWindowBuilder, Window, WindowEvent,
 };
 
@@ -673,39 +673,24 @@ pub fn run() {
                 None,
             ))?;
 
-            let open_item = MenuItem::with_id(app, "open", "Open ClipB", true, None::<&str>)?;
-            let quick_item = MenuItem::with_id(app, "quick", "Quick Copy", true, None::<&str>)?;
+            let launch_item = MenuItem::with_id(app, "launch", "Launch ClipB", true, None::<&str>)?;
             let quit_item = MenuItem::with_id(app, "quit", "Quit ClipB", true, None::<&str>)?;
 
-            let menu = Menu::with_items(app, &[&open_item, &quick_item, &quit_item])?;
+            let menu = Menu::with_items(app, &[&launch_item, &quit_item])?;
 
             TrayIconBuilder::with_id("clipb-tray")
                 .tooltip("ClipB")
                 .icon(app.default_window_icon().unwrap().clone())
                 .menu(&menu)
-                .show_menu_on_left_click(false)
+                .show_menu_on_left_click(true)
                 .on_menu_event(|app, event| match event.id.as_ref() {
-                    "open" => {
+                    "launch" => {
                         let _ = show_main(app);
-                    }
-                    "quick" => {
-                        let _ = toggle_quick(app);
                     }
                     "quit" => {
                         app.exit(0);
                     }
                     _ => {}
-                })
-                .on_tray_icon_event(|tray, event| {
-                    if let TrayIconEvent::Click {
-                        button: MouseButton::Left,
-                        button_state: MouseButtonState::Up,
-                        ..
-                    } = event
-                    {
-                        let app = tray.app_handle();
-                        let _ = show_main(app);
-                    }
                 })
                 .build(app)?;
 
